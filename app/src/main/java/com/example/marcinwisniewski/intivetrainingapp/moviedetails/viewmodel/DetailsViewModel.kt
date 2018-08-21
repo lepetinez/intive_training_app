b37da6eedd155c9ed1956879b8e6312da4f5f94b
lepetinez <lepetinez@gmail.com>
2018-08-21 11:40:13 +0200

    all changes in one commit (#3)
    Last change will be done in the first commit on new branch

--- app/src/main/java/com/example/marcinwisniewski/intivetrainingapp/moviedetails/viewmodel/DetailsViewModel.kt
+++ app/src/main/java/com/example/marcinwisniewski/intivetrainingapp/moviedetails/viewmodel/DetailsViewModel.kt
@@ -1,5 +1,13 @@
 package com.example.marcinwisniewski.intivetrainingapp.moviedetails.viewmodel
 
+import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
+import com.example.marcinwisniewski.intivetrainingapp.movielist.model.Movie
 
-class DetailsViewModel : ViewModel()
+class DetailsViewModel : ViewModel() {
+    val choosenMovie: MutableLiveData<Movie> = MutableLiveData()
+    fun fetchChoosenMovie(choosenMovie: Movie?) {
+        // TODO: after api change, this function will fetch real live data from the api (argument will be movie_ID)
+        this.choosenMovie.value = choosenMovie
+    }
+}
--- app/src/main/java/com/example/marcinwisniewski/intivetrainingapp/movielist/viewmodel/SingleItemViewModel.kt
+++ app/src/main/java/com/example/marcinwisniewski/intivetrainingapp/movielist/viewmodel/SingleItemViewModel.kt
@@ -1,6 +1,16 @@
 package com.example.marcinwisniewski.intivetrainingapp.movielist.viewmodel
 
-import android.arch.lifecycle.ViewModel
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.findNavController
+import com.example.marcinwisniewski.intivetrainingapp.R
 import com.example.marcinwisniewski.intivetrainingapp.movielist.model.Movie
 
-data class SingleItemViewModel(val movie: Movie) : ViewModel()
+data class SingleItemViewModel(val movie: Movie) {
+    fun showMovieDetails(movie: Movie, view: View) {
+        val movieBundle = Bundle().apply{
+            putSerializable(view.context.getString(R.string.current_movie), movie)
+        }
+        view.findNavController().navigate(R.id.detailsFragment, movieBundle)
+    }
+}
