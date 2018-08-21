c71c424a2faab622bf12a441cc21b495486fbe3b
marcin.wisniewski <lepetinez@gmail.com>
2018-08-21 13:03:26 +0200

    dawew

--- app/src/main/java/com/example/marcinwisniewski/intivetrainingapp/dagger/dagger/component/App.kt
+++ app/src/main/java/com/example/marcinwisniewski/intivetrainingapp/dagger/dagger/component/App.kt
@@ -2,16 +2,21 @@ package com.example.marcinwisniewski.intivetrainingapp.dagger.dagger.component
 
 import android.app.Activity
 import android.app.Application
+import android.support.v4.app.Fragment
 import com.example.marcinwisniewski.intivetrainingapp.dagger.dagger.module.AppModule
 import com.example.marcinwisniewski.intivetrainingapp.dagger.dagger.module.NetworkModule
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasActivityInjector
+import dagger.android.HasFragmentInjector
+import dagger.android.support.HasSupportFragmentInjector
 import javax.inject.Inject
 
-class App : Application(), HasActivityInjector {
-    @Inject
+class App : Application(), HasActivityInjector, HasSupportFragmentInjector
     lateinit var activityInjector: DispatchingAndroidInjector<Activity>
+    @Inject
+    lateinit var fragmentInjector: DispatchingAndroidInjector<Fragment>
+    override fun supportFragmentInjector(): AndroidInjector<Fragment> = fragmentInjector
     override fun activityInjector(): AndroidInjector<Activity> = activityInjector
     override fun onCreate() {
         super.onCreate()
