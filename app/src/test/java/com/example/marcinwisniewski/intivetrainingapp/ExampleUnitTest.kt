da116347294b29a0991ec759ae633c4a353a941b
marcin.wisniewski <lepetinez@gmail.com>
2018-08-30 17:05:15 +0200

    adawe

--- app/src/test/java/com/example/marcinwisniewski/intivetrainingapp/ExampleUnitTest.kt
+++ app/src/test/java/com/example/marcinwisniewski/intivetrainingapp/ExampleUnitTest.kt
@@ -1,8 +1,13 @@
 package com.example.marcinwisniewski.intivetrainingapp
 
+import android.arch.core.executor.testing.InstantTaskExecutorRule
+import com.example.marcinwisniewski.intivetrainingapp.movielist.model.MovieRepository
+import com.example.marcinwisniewski.intivetrainingapp.movielist.viewmodel.MovieListViewModel
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.Rule
+import org.mockito.Mock
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,6 +15,16 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
+    @Rule
+    @JvmField
+    val rule = InstantTaskExecutorRule()
+
+    lateinit var listViewModel: MovieListViewModel
+
+    @Mock
+    lateinit var appDatabase: MovieRepository
+
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
